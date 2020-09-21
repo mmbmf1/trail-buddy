639dbf9d0b75474f0650c962de879458be01c010
Michael Mace <mmbmf.mm@gmail.com>
2020-09-21 10:52:43 -0500

    initial commit

--- src/components/Trails.jsx
+++ src/components/Trails.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Weather from '../components/Weather'
 
 const Location = (props) => {
   return (
     <div>
       <section id="trail-results" class="hidden">
         <h2>Search Results</h2>
-        <ul id="js-trail-results-list"></ul>
         <button type="submit" id="home-button">
           Return to Search
         </button>
